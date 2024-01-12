350e8b91dc9c3f73a4f1e42c2cf8a75d6d89abd2
AndreaGul <andreagulino99@gmail.com>
2024-01-12 03:09:12 +0100

    feat: scrittura dei dati presi dal server in pagina

--- src/js/main.js
+++ src/js/main.js
@@ -3,26 +3,14 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      discs: [
-        {
-          disco: 'nome1',
-          img: 'img1',
-        },
-        {
-          disco: 'nome2',
-          img: 'img2',
-        },
-        {
-          disco: 'nome3',
-          img: 'img3',
-        },
-      ],
+      discs: [],
     };
   },
   methods: {
     getDiscs() {
       axios.get('server.php').then((response) => {
         console.log({ response });
+        this.discs = response.data;
       });
     },
   },
